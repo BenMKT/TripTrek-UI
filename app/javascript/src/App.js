9389d1d6a9e75b466e8dac0a94b5b863b1886ee9
devalibello <alibelloimoukhuede@gmail.com>
2023-12-02 13:55:21 +0300

    Add src folder for react

---
+++ app/javascript/src/App.js
@@ -0,0 +1,15 @@
+// The App.js will contain all the components and the route for the react app
+
+// Example:
+
+// const App = () => (
+//     <div>
+//       <BrowserRouter>
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<Main />} />
+//           <Route path="/details/*" element={<Details />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
---
+++ app/javascript/src/index.js
@@ -0,0 +1,4 @@
+// The indexedDB.js will render the dom to the html tag with id=root
+// Example
+// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
