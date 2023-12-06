41fe7e1640b978ed162c4dc325334a84bbb9ec64
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-12-06 15:46:54 +0200

    add list to delete

--- src/root/Root.js
+++ src/root/Root.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Root = () => (
-  <div className='container-root'>
+  <div className="container-root">
     <div className="nav">
       nav
     </div>
