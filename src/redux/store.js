77107532907557fab5f42743b183c5848c55858a
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-12-09 15:56:40 +0430

    Update store file, Navigation link, and add localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './cars/carsSlice';
 
+
 const store = configureStore({
   reducer: {
     cars: carsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localStorageMiddleware),
 });
 
 export default store;
