bec46f570acc51d845c42f292dafc31fd5e11296
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-12-11 13:10:05 +0200

    add car to backend

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -4,4 +4,14 @@ import axios from 'axios';
 const fetchCars = createAsyncThunk('cars/fetchCars',
   async (thunkApi) => thunkApi);
 
-export { fetchCars };
+const addACar = createAsyncThunk('cars/addACar', async (car, thunkAPI) => {
+  const url = `http://localhost:3000/api/v1/cars`;
+  try {
+    const response = await axios.post(url, car);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('something went wrong');
+  }
+});
+
+export { fetchCars, addACar };
