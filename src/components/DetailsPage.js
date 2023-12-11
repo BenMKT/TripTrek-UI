cb6779635b0456f0cd22e3c5f799fa6f9636e974
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-12-12 00:50:09 +0200

    update item to Detele

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -3,66 +3,71 @@ import { Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import carImg from '../assets/images/car.png';
 import ReserveCar from './ReserveCar';
 import '../styles/carDetails.css';
 
-const DetailsPage = () => (
-  <Container className="details-container">
-    <div>
-      <Card className="img-container">
-        <Card.Img variant="top" src={carImg} alt="car image" />
-      </Card>
-      <Card className="car-features">
-        <Card.Header>
-          <h2>Car s Model</h2>
-          <em>Price deposit upon any car purchase</em>
-        </Card.Header>
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <p className="key">Finance fee</p>
-            <p className="value">123</p>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <p className="key">Option to purchase fee</p>
-            <p className="value">123</p>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <p className="key">amount payable</p>
-            <p className="value">1234</p>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <p className="key">Duration</p>
-            <p className="value">1234</p>
-          </ListGroup.Item>
-        </ListGroup>
-        <Card.Body>
-          <Card.Text>
-            <strong>5.9% APR</strong>
-            {' '}
-            Representative
-          </Card.Text>
-          <div className="links">
-            <div>
-              <Link to="/">
-                DISCOVER MORE MODELS
-                <i className="bi bi-chevron-compact-right" />
-              </Link>
-              <strong>Image</strong>
+const DetailsPage = (props) => {
+  const car = props;
+  return (
+    <Container className="details-container">
+      <div>
+        <Card className="img-container">
+          <Card.Img variant="top" src={car.photo} alt="car image" />
+        </Card>
+        <Card className="car-features">
+          <Card.Header>
+            <h2>{car.model}</h2>
+            <em>Price deposit upon any car purchase</em>
+          </Card.Header>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <p className="key">Finance fee</p>
+              <p className="value">{car.finance_fee}</p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p className="key">Option to purchase fee</p>
+              <p className="value">{car.purchase_fee}</p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p className="key">amount payable</p>
+              <p className="value">1234</p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p className="key">Duration</p>
+              <p className="value">{car.amount_payable}</p>
+            </ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Card.Text>
+              <strong>
+                {car.apr}
+                {' '}
+                APR
+              </strong>
+              {' '}
+              Representative
+            </Card.Text>
+            <div className="links">
+              <div>
+                <Link to="/">
+                  DISCOVER MORE MODELS
+                  <i className="bi bi-chevron-compact-right" />
+                </Link>
+                <strong>Image</strong>
+              </div>
+              <ReserveCar />
             </div>
-            <ReserveCar/>
-          </div>
-        </Card.Body>
-      </Card>
-    </div>
-    <Link
-      to="/"
-      className="link-back"
-    >
-      <i className="bi bi-caret-left" />
-    </Link>
-  </Container>
-);
+          </Card.Body>
+        </Card>
+      </div>
+      <Link
+        to="/"
+        className="link-back"
+      >
+        <i className="bi bi-caret-left" />
+      </Link>
+    </Container>
+  );
+};
 
 export default DetailsPage;
--- src/components/ListToDelete.js
+++ src/components/ListToDelete.js
@@ -1,26 +1,45 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Container } from 'react-bootstrap';
+import { v4 as uuidv4 } from 'uuid';
 import ItemToDelete from './ItemToDelete';
 import '../styles/itemToDelete.css';
 import SearchBar from './SearchBar';
 
-const ToDeleteList = () => (
-  <Container className="container-list-to-delete">
-    <header>
-      <h1>Delete a car</h1>
-    </header>
-    <SearchBar />
-    <div>
-      <ul>
-        <li><ItemToDelete /></li>
-        <li><ItemToDelete /></li>
-        <li><ItemToDelete /></li>
-        <li><ItemToDelete /></li>
-        <li><ItemToDelete /></li>
-      </ul>
-    </div>
-  </Container>
+const ToDeleteList = () => {
+  const { cars, isLoading, error } = useSelector((store) => store.cars);
+  const listOfCars = cars.map((car) => (
+    <li key={uuidv4()}>
+      <ItemToDelete car={car} />
+    </li>
+  ));
 
-);
+  let content;
+  if (isLoading) {
+    content = <div>Is Loading...</div>;
+  } else if (error) {
+    content = <div>something went wrong</div>;
+  } else if (cars.length) {
+    content = (
+      <ul>
+        {listOfCars}
+      </ul>
+    );
+  } else {
+    content = <div>No car yet</div>;
+  }
+  return (
+    <Container className="container-list-to-delete">
+      <header>
+        <h1>Delete a car</h1>
+      </header>
+      <SearchBar />
+      <div>
+        {content}
+      </div>
+    </Container>
+
+  );
+};
 
 export default ToDeleteList;
