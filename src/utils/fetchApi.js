7142a210d4954720f206f9c36c41effd23cf67d5
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-12-11 18:37:49 +0200

    update request

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -5,16 +5,21 @@ const fetchCars = createAsyncThunk('cars/fetchCars',
   async (thunkApi) => thunkApi);
 
 const addACar = createAsyncThunk('cars/addACar', async (car, thunkAPI) => {
-  console.log('step 1')
   const url = `http://localhost:3000/api/v1/cars`;
   try {
-    console.log('step 2')
-    const response = await axios.post(url, car);
-    console.log("la reponse est ${response}")
+    //console.log(thunkAPI.getState())
+    const { user } = thunkAPI.getState();
+    console.log(user)
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${user.auth.token}`, 
+    };
+    console.log(user.auth.token.split('.').length)
+    const response = await axios.post(url, car, { headers });
     return response.data;
   } catch (error) {
-    console.log('step 4')
-    return thunkAPI.rejectWithValue('something went wrong');
+    console.log(error)
+    return thunkAPI.rejectWithValue(`something went wrong: ${error.response.data}`);
   }
 });
 
