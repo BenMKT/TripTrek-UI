79438636b7b72efce75f709ff97747de4241ca6d
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-12-12 15:42:51 +0200

    persit data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import carsReducer from './cars/carsSlice';
 import userReducer from './users/userSlice';
 import localStorageMiddleware from '../middleware/localStorage';
 
+const persistConfig = {
+  key: 'root', // key for the persist
+  storage, // storage we're using, localStorage in our case
+};
+
+const rootReducer = combineReducers({
+  // combine the cars and user reducers into a single root reducer
+  cars: carsReducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); // the actual reducer
+
 const store = configureStore({
-  reducer: {
-    cars: carsReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localStorageMiddleware),
 });
 
 export default store;
+export const persistor = persistStore(store);
