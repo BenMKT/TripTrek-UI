8734b096debaee149a5ed549088e7ae6b23d8f60
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-12-12 18:00:44 +0430

    Update store file for adding navigation

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './cars/carsSlice';
 import userReducer from './users/userSlice';
 import localStorageMiddleware from '../middleware/localStorage';
+import reservationReducer  from './reservations/reservationsSlice';
 
 const store = configureStore({
   reducer: {
     cars: carsReducer,
     user: userReducer,
+    reservations: reservationReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localStorageMiddleware),
 });
