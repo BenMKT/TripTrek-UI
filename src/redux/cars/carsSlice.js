b7a21364ed77a4d8913724d9867ed9994fc35ac4
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-12-13 14:58:38 +0200

    resize cars List

--- src/redux/cars/carsSlice.js
+++ src/redux/cars/carsSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchCars, addACar, removeACar } from '../../utils/fetchApi';
+import { useDispatch } from 'react-redux';
 
 const name = 'cars';
+
 const initialState = {
   cars: [],
   isLoading: false,
@@ -17,30 +19,40 @@ const carsSlice = createSlice({
     builder
       .addCase(addACar.pending, (state) => {
         state.isLoading = true;
+        state.error = '';
+        state.message = '';
       })
       .addCase(addACar.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.message = 'Car added';
+        state.error = '';
+        state.message = 'Car added ✔️';
         state.cars.push(action.payload);
       })
       .addCase(addACar.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
+        state.message = '';
       })
       .addCase(removeACar.pending, (state) => {
         state.isLoading = true;
+        state.error = '';
+        state.message = '';
       })
       .addCase(removeACar.fulfilled, (state, action) => {
         state.isLoading = false;
         state.cars = state.cars.filter((car) => car.id !== action.payload);
+        state.message = 'Car deleted ✔️'
         state.error = ''
       })
       .addCase(removeACar.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
+        state.message = '';
       })
       .addCase(fetchCars.pending, (state) => {
         state.isLoading = true;
+        state.message = '';
+        state.error = ''
       })
       .addCase(fetchCars.fulfilled, (state, action) => {
         state.isLoading = false;
